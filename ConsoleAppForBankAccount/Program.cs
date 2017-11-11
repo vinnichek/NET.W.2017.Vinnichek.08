6a57b41a149f4399d08b9533db8170f8555ca0df
ivinnichek <vinnichekira@gmail.com>
2017-11-12 02:40:59 +0300

    Edit ConsoleUI for Book

--- ConsoleAppForBankAccount/Program.cs
+++ ConsoleAppForBankAccount/Program.cs
@@ -1,46 +1,72 @@
 ﻿using System;
-using BankAccount.Logic;
-using BankAccount.Logic.BankAccountStorage;
+using Book.Logic.BookStorage;
+using static Book.Logic.ComparerMethods;
+using Book.Logic;
+using static Book.Logic.Finder.FinderMethods;
+using System.Globalization;
 
-namespace ConsoleAppForBankAccount
+namespace ConsoleAppForBook
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            BaseBankAccount firstBankAccount = new BaseBankAccount(1234, "Vinnichek Ira");
-            GoldBankAccount secondBankAccount = new GoldBankAccount(3456, "Petrov Petya");
-            PlatinumBankAccount thirdBankAccount = new PlatinumBankAccount(4567, "Ivanov Ivan");
-            PlatinumBankAccount fourthBankAccount = new PlatinumBankAccount(4567, "Ivanov Ivan");
+            Book.Logic.Book firstBook = new Book.Logic.Book("12345", "Suzanne Collins", "The Hunger Games", "Scholastic Press",
+                                     2008, 234, 20);
+            Book.Logic.Book secondBook = new Book.Logic.Book("23456", "J.K. Rowling", "Harry Potter and the Order of the Phoenix", "Scholastic Inc.",
+                                     2004, 312, 17);
+            Book.Logic.Book thirdBook = new Book.Logic.Book("34567", "Harper Lee", "To Kill a Mockingbird", "Harper Perennial Modern Classics",
+                                     2006, 245, 21);
+            Book.Logic.Book fourthBook = new Book.Logic.Book("45678", "Jane Austen", "Pride and Prejudice", "Modern Library",
+                                     2000, 634, 18);
+            Book.Logic.Book fifthBook = new Book.Logic.Book("56789", "Stephenie Meyer", "Twilight", "Little, Brown and Company ",
+                                     2006, 134, 25);
+            Book.Logic.Book sixthBook = new Book.Logic.Book("56789", "Stephenie Meyer", "Twilight", "Little, Brown and Company ",
+                                    2006, 134, 25);
 
-            Console.WriteLine(firstBankAccount);
+            Console.WriteLine(firstBook.ToString());
 
-            Console.WriteLine(firstBankAccount.Equals(secondBankAccount)); //false
-            Console.WriteLine(thirdBankAccount.Equals(fourthBankAccount)); //true
+            IFormatProvider fp = new BookFormatProvider();
+            Console.WriteLine(string.Format(fp, "{0:AT}", firstBook));
 
-            firstBankAccount.ReplenishBalance(1000);
-            secondBankAccount.ReplenishBalance(1000);
-            thirdBankAccount.ReplenishBalance(1000);
-            thirdBankAccount.WithdrawMoney(200);
+            Console.WriteLine(fifthBook);
 
-            BankAccountService service = new BankAccountService();
-            service.AddBankAccount(firstBankAccount);
-            service.AddBankAccount(secondBankAccount);
-            Console.WriteLine(service);
+            Console.WriteLine(fifthBook.Equals(sixthBook)); //true
+            Console.WriteLine(firstBook.Equals(secondBook)); //false
 
-            service.RemoveBankAccount(secondBankAccount);
-            Console.WriteLine(service);
+            Console.WriteLine(fifthBook.CompareTo(thirdBook)); //1
+            Console.WriteLine(sixthBook.CompareTo(fifthBook)); //0
 
-            service.AddBankAccount(secondBankAccount);
-            service.AddBankAccount(thirdBankAccount);
+            BookListService service = new BookListService();
+            service.AddBook(firstBook);
+            service.AddBook(secondBook);
+            service.AddBook(thirdBook);
+            //service.AddBook(thirdBook); //exeption
+            service.RemoveBook(thirdBook);
+            service.AddBook(thirdBook);
+            service.AddBook(fourthBook);
+            service.AddBook(fifthBook);
 
             Console.WriteLine("\r\n");
-            Console.WriteLine("------List of bank accounts.------");
+            Console.WriteLine("------List of books.------");
             Console.WriteLine(service);
 
-            IBankAccountStorage binaryStorage = new BankAccountBinaryStorage(@"/Users/vinnichek/Projects/Task/ConsoleAppForBankAccount/BankAccounts.txt"); 
+            IBookStorage binaryStorage = new BookBinaryStorage(@"/Users/vinnichek/Projects/Task/ConsoleAppForBook/Books.txt");
             //service.SaveToStorage(binaryStorage);
             //service.LoadFromStorage(binaryStorage);
+
+            Console.WriteLine(service.FindBookByTag(new FindBookByAuthor("Suzanne Collins")));
+            Console.WriteLine(service.FindBookByTag(new FindBookByPrice(25)));
+
+            Console.WriteLine("\r\n");
+            Console.WriteLine("------Sorting by authors.------");
+            service.SortBookByTag(new CompareByAuthor());
+            Console.WriteLine(service);
+
+            Console.WriteLine("\r\n");
+            Console.WriteLine("------Sorting by price.------");
+            service.SortBookByTag(new CompareByPrice());
+            Console.WriteLine(service);
         }
     }
 }
